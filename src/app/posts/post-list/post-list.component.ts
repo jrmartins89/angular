b5961c36bd9d62fe507f78aa1530c6480b5d4a04
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2021-03-25 19:45:19 -0300

    added margin-top and margin to the component.css, edited the component.html to rearrange the Angular component, included the MatExpansionModule, edited the post-list.component.css to include display and margin-top, edited the post-list.component.html to include the MatExpansion components

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -8,4 +8,10 @@ import { Component } from "@angular/core";
   }
 )
 
-export class PostListComponent{}
+export class PostListComponent{
+  posts = [
+    {title:'first post',  content:'This is the first post!'},
+    {title:'second post',  content:'This is the second post!'},
+    {title:'third post',  content:'This is the third post!'}
+  ]
+}
