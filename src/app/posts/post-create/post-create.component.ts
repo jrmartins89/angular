0e1ed25a090ba612cc853e2fd987cee7e9170d25
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2021-03-25 15:41:09 -0300

    adding component files for the post-create and post-list angular components

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component(
+  {
+    selector: 'app-post-create',
+    styleUrls:['./post-create/post-create.component.css']
+  }
+)
+
+export class PostCreate{}
