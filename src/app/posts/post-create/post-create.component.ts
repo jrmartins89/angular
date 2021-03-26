ea458dc921936d95de584094265bab43bcbe98e4
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2021-03-25 23:56:41 -0300

    implemented the posts.service component in the post-create component and the post-list component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter} from "@angular/core";
 import { NgForm } from "@angular/forms";
 import {Post} from '../post.model';
+import { PostsService } from "../posts.service";
 
 @Component(
   {
@@ -14,19 +15,17 @@ export class PostCreateComponent{
   newPost="";
   enteredContent="";
   enteredTitle="";
+  postCreated = new EventEmitter<Post>();
   //EventEmitter is a generic type, which means we can pass additional information about which type of data it
   //works with. To pass this information, we need to add the <> and add the type between them
-  @Output() postCreated= new EventEmitter<Post>();
+
+  constructor(public PostsService:PostsService){}
 
   onAddPost(form:NgForm){
     //checking to see if the form object is valid or not. this will prevent invalid posts to be added to the post list
     if(form.invalid){
       return;
     }
-    const post:Post = {
-      title: form.value.title,
-      content:form.value.content
-    }
-    this.postCreated.emit(post);
+    this.PostsService.addPosts(form.value.title, form.value.content);
   };
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input} from "@angular/core";
+import { Component, OnDestroy, OnInit} from "@angular/core";
 import {Post} from '../post.model';
+import {Subscription} from 'rxjs';
 import { PostsService } from "../posts.service";
 
 @Component(
@@ -10,14 +11,29 @@ import { PostsService } from "../posts.service";
   }
 )
 
-export class PostListComponent{
+export class PostListComponent implements OnInit, OnDestroy{
 /*   post_list = [
     {title:'first post',  content:'This is the first post!'},
     {title:'second post',  content:'This is the second post!'},
     {title:'third post',  content:'This is the third post!'}
   ] */
-  @Input() post_list: Post[]=[];
+  private postsSub: Subscription;
+  post_list: Post[]=[];
 
-  //the public keyword will automatically create a new property in the component and store the incoming value in the property
+  //the public keyword will automatically create a new property in the
+  //component and store the incoming value in the property
   constructor(public postsService: PostsService){ }
+
+  ngOnInit(){
+    this.post_list = this.postsService.getPosts();
+    //we need to add a argument to the function subscribe.
+    //It's a function that will be called whenever a new value is received
+    this.postsSub= this.postsService.getPostsUpdateListener().subscribe((posts:Post[])=>{
+      this.post_list=posts;
+    });
+  }
+
+  ngOnDestroy(){
+    this.postsSub.unsubscribe();
+  }
 }
